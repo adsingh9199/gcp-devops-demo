a15542c30c8a7aab31eb91ea6f67451c1a1e0e00
adsingh007 <virdiaman07@gmail.com>
2026-09-25 15:20:10 +0530

    Initial GCP DevOps demo

---
+++ app/test_app.py
@@ -0,0 +1,26 @@
+from app import app
+
+
+def test_home():
+    client = app.test_client()
+
+    response = client.get("/")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["application"] == "GCP DevOps Demo"
+    assert data["environment"] == "DEV"
+
+
+def test_health():
+    client = app.test_client()
+
+    response = client.get("/health")
+
+    assert response.status_code == 200
+
+    data = response.get_json()
+
+    assert data["status"] == "healthy"
